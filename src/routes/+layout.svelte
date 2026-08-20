ce5e33c8d94727c96a6fab06c416a5d609c68e4c
Kiri <cultivatingghoul@gmail.com>
2026-08-20 17:51:15 +0200

    added favicon

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -5,5 +5,19 @@
 	let { children } = $props();
 </script>
 
-<svelte:head><link rel="icon" href={favicon} /></svelte:head>
+<svelte:head>
+	<title>Christin Nyakanyanga</title>
+		<meta
+		name="description"
+		content="Merge magical elements on a dark runic grid. Discover 800+ unique emoji recipes in Grimoji, the ultimate gothic alchemy puzzle game combining 2048 and match-3 mechanics."
+	/>
+	<meta
+		name="keywords"
+		content="Christin Nyakanyanga, grimoji maker, Destoryer's Fury creator, Kai's Genesis author, Ghetto Coders, Fluent Epub Maker"
+	/>
+	<link rel="canonical" href="https://grimoji.io" />
+	<link rel="icon" href={favicon} />
+</svelte:head>
 {@render children()}
+
+
