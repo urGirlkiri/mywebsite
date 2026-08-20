d4f79618401b663c2c12ed02b7a94e04dfe440f8
Kiri <cultivatingghoul@gmail.com>
2026-08-20 07:36:47 +0200

    added zeni  to the page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,1 +1,18 @@
-<h1>Welcome to My Website</h1>
+<script>
+    	import { resolve } from '$app/paths';
+
+</script>
+
+
+<nav class="flex justify-between bg-white text-shadow- text-shadow-black-9xs mt-4 mb-18 px-4 border-pink border-y-2 font-Quicksand *:not-[]:text-lg uppercase skew-0">
+    <a class="hover:text-blue" href={resolve('/')}>Moi</a>
+    <a class="hover:text-blue" href={resolve('/')}>Projects</a>
+    <a class="hover:text-blue" href={resolve('/')}>more info</a>
+</nav>
+<section class="flex flex-col">
+	<div class="self-center bg-yellow shadow-lg w-100 h-50 rotate-14 shblackadow-black">
+        <img src="/zeni.png" alt="zeno" class="w-150" >
+    </div>
+</section>
+
+
